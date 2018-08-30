1d89164dbe442989e719d987e26735dee778075d
nicktrajkovski <trajkovskinick@gmail.com>
2018-08-30 22:33:37 +1000

    second commit

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,6 +1,23 @@
 @extends('layouts.app')
 
 @section('content')
+
+<head>
+  <script>
+    function addFields(){
+      var container = document.getElementbyId("addSkillsField");
+      //for (i=0, i<){
+        var input = document.createElement("input");
+        input.type = "text";
+        input.name = "skill"; // + i
+        input.placeholder = "skill"; //+ i
+        container.appendChild(input);
+        container.appendChild(document.createElement("br"));
+      //}
+    }
+  </script>
+</head>
+
 <div class="container">
     <div class="row">
       <div class="container-fluid">
@@ -17,21 +34,46 @@
                 </div>
                 <br>
                 <div class="panel-body" align="center">
-                    <p>[Profile Picture]</p>
+                    <p>[Profile Picture]</p> <input name="picture" id="picture" disabled></input>
                 </div>
-                <bbr>
+                <br><br>
             <div class="panel panel-default" align="left">
               <br>
                 <div class="panel-body" align="left">
                   <form action="" method="POST">
-                    <p>Skills</p> <textarea rows="4" col="50"> </textarea> <br> <br>
-                    <p>Education</p> <textarea rows="4" col="50"> </textarea> <br> <br>
-                    <p>Current Job</p> <textarea rows="4" col="50"> </textarea> <br> <br>
-                    <p>Previous Job</p> <textarea rows="4" col="50"> </textarea> <br> <br>
-                    <p>Location</p> <textarea rows="4" col="50"> </textarea> <br> <br>
-                    <p>Area of Work (e.g. IT,Engineering, etc)</p> <textarea rows="4" col="50"> </textarea> <br> <br>
-                    <p>Job Preference</p> <textarea rows="4" col="50"> </textarea>
-                  </form>
+                    <h5>Skills</h5>
+                    <input type="text" name="skill_1" placeholder="Skill 1"> <br> <br>
+                    <input type="text" name="skill_2" placeholder="Skill 2"> <br> <br>
+                    <input type="text" name="skill_3" placeholder="Skill 3"> <br> <br>
+                    <input type="text" name="skill_4" placeholder="Skill 4"> <br> <br>
+                    <div id="addSkillsField" />
+                    <br> <br>
+                    <button type="button" onclick="addFields();">Add More Skills</button>
+
+                    <br> <br> <br>
+                    <h6>Click here to submit your <b>skills</b></h6>
+                    <input type="submit" value="Save"></input>
+                    </form>
+                </div>
+              <br>
+            </div>
+          </div>
+          <div class="jumbotron">
+              <div class="panel-body" align="center">
+
+                <div class="panel-body" align="left">
+                  <form action="" method="POST">
+                    <h5>Education</h5> <textarea rows="4" col="50" placeholder="Education"> </textarea> <br> <br>
+                    <h5>Current Job</h5> <textarea rows="4" col="50" placeholder="Current Job"> </textarea> <br> <br>
+                    <h5>Previous Job</h5> <textarea rows="4" col="50" placeholder="Previous Job"> </textarea> <br> <br>
+                    <h5>Location</h5> <textarea rows="4" col="50" placeholder="Location"> </textarea> <br> <br>
+                    <h5>Area of Work (e.g. IT,Engineering, etc)</h5> <textarea rows="4" col="50" placeholder="Area of Work"> </textarea> <br> <br>
+                    <h5>Job Preference</h5> <textarea rows="4" col="50" placeholder="Job Preference"> </textarea> <br><br>
+
+                    <br> <br>
+                    <h6>Click here to submit your <b>profile information</b></h6>
+                    <input type="submit" value="Save"></input>
+                    </form>
                 </div>
             </div>
           </div>
