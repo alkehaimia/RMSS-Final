5a8bef0165e346198765e1faa9f10076e08225df
nicktrajkovski <trajkovskinick@gmail.com>
2018-09-10 21:21:51 +1000

    profile updated

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -3,25 +3,7 @@
 @section('content')
 
 <head>
-  <script type='text/javascript'>
-    function addFields(){
-
-      var number = document.getElementbyId("numberOfSkills").value;
-
-      var container = document.getElementbyId("addSkillsField");
-
-      for (i = 0; i < number; i++)
-      {
-        container.appendChild(document.createTextNode(i + 1));
-        var input = document.createElement("input");
-        input.type = "text";
-        input.name = "skill" + i ;
-        input.placeholder = "Skill" + i;
-        container.appendChild(input);
-        container.appendChild(document.createElement("br"));
-      }
-  }
-  </script>
+  <!--<script src="{{ asset('js/createSkillsInput.js') }}" defer></script>-->
 </head>
 
 <div class="container">
