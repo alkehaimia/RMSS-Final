4145d21fd1ee35edcbce7696e5b6700a490d206e
s3599962 <s3599962@student.rmit.edu.au>
2018-09-24 23:17:25 +1000

    Nick's part

---
+++ app/ProfileSkills.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ProfileSkills extends Model
+{
+
+  protected $table = 'profileSkills';
+  public $primaryKey = 'id';
+  public $timestamps = false;
+
+}
--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,47 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', function (){
+    return view('index');
+})->name('index');
 
+Route::get('/home', function (){
+    return view('index');
+})->name('index');
+
+
+//Route::get('/', function () {
+  //  return view('index');
+//});
+
+Route::get('/profile', function(){
+    return view('profile');
+})->name('profile');
+
+Route::get('/displayedProfile', function(){
+    return view('displayedProfile');
+})->name('displayedProfile');
+
+Route::get('/profileSkills', function(){
+    return view('profileSkills');
+})->name('profileSkills');
+
+Route::get('/profileEducation', function(){
+    return view('profileEducation');
+})->name('profileEducation');
+
+Route::resource('UserProfile', 'ProfileController');
+Route::resource('ProfileSkills', 'ProfileSkillsController');
+Route::resource('ProfileEducation', 'ProfileEducationController');
+
+/*
+Route::resource('profile', 'ProfileController');
+Route::resource('profile_skills', 'ProfileSkillsController');
+Route::resource('profile_education', 'ProfileEducationController');
+*/
+
+//Authentication Routes
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+//Route::get('/home', 'HomeController@index')->name('home');
+?>
